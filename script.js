e91c68ac3c56c72a83798461aafbb60b36a83090
Chinnam-JanakiDevi <chinnam.janakidevi123@gmail.com>
2024-02-18 10:29:09 +0530

    portfolio

--- script.js
+++ script.js
@@ -1,15 +1,41 @@
-function display(k) {
-    if (k == "skills") {
-      document.getElementById("experience").style.display = "none";
-      document.getElementById("education").style.display = "none";
-      document.getElementById("skills").style.display = "block";
-    } else if (k == "experience") {
-      document.getElementById("skills").style.display = "none";
-      document.getElementById("education").style.display = "none";
-      document.getElementById("experience").style.display = "block";
-    } else if (k == "education") {
-      document.getElementById("experience").style.display = "none";
-      document.getElementById("skills").style.display = "none";
-      document.getElementById("education").style.display = "block";
-    }
+ /**
+   * Initiate portfolio lightbox 
+   */
+ const portfolioLightbox = GLightbox({
+  selector: '.portfolio-lightbox'
+});
+
+/**
+ * Testimonials slider
+ */
+new Swiper('.testimonials-slider', {
+  speed: 600,
+  loop: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false
+  },
+  slidesPerView: 'auto',
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true
   }
+});
+
+/**
+ * Portfolio details slider
+ */
+new Swiper('.portfolio-details-slider', {
+  speed: 400,
+  loop: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true
+  }
+});
